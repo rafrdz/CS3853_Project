dca08f097df8bcd0e8994695da128bd4322ea6e3
Rafael Rodriguez <rrodriguez1989@gmail.com>
2019-03-20 18:54:45 -0500

    Work on milestone 1

--- milestone1.py
+++ milestone1.py
@@ -2,43 +2,119 @@
 
 import sys
 import os
+import argparse
+import math
+import re
 
 # CS3853 Computer Architecture Project
 # Milestone 1
-# Team 6: Rafael Rodriguez,
+# Team 6: Rafael Rodriguez, David Brenner, James Solis
 
 
-def get_all_files(directory):
-    file_list = []
-    for file in os.listdir(directory):
-        if file.endswith('.trc'):
-            file_list.append(file)
-    return file_list
+def determine_valid_file(string):
+    if os.path.isfile(string):
+        return string
+    else:
+        raise argparse.ArgumentTypeError('file ' + string + ' does not exist')
 
 
-def parse_files(files_list):
-    for file in files_list:
-        try:
-            with open(sys.argv[1] + '/' + file, 'r') as f:
-                line_count = 0
-                for line in f:
-                    line_count += 1
-                print('Number of lines in ' + file + ' is: ' + str(line_count))
-        except FileNotFoundError:
-            print('Error: File was not found')
-            print('Please check that the file exists and try again')
-            sys.exit(2)
+def cache_size_type(string, min=1, max=8192):
+    value = int(string)
+    if min <= value <= max:
+        return value
+    else:
+        raise argparse.ArgumentTypeError('value not in range ' + str(min) + ' - ' + str(max))
 
 
-# Verify the number of arguments
-if len(sys.argv) != 3:
-    print('Error: Not enough arguments')
-    print('Usage: [python file] [data directory] [output directory]')
-    sys.exit(1)
+def block_size_type(string, min=4, max=64):
+    value = int(string)
+    if min <= value <= max:
+        return value
+    else:
+        raise argparse.ArgumentTypeError('value not in range ' + str(min) + ' - ' + str(max))
 
-# Create the output directory if it does not exist
-if not os.path.exists(sys.argv[2]):
-    os.mkdir(sys.argv[2])
+
+def determine_block_offset():
+    return int(math.log(results.block_size, 2))
+
+
+def determine_index_size():
+    return int(math.log(results.cache_size, 2) - (math.log(results.block_size, 2) + math.log(results.associativity, 2)))
+
+
+def determine_indices():
+    return int(results.cache_size/(results.block_size * results.associativity))
+
+
+def print_formatted_header():
+    print('Cache Simulator CS 3853 Spring 2019 - Group #006')
+    print()
+    print('Cmd Line: ' + ' '.join(sys.argv))
+    print('Trace File: ' + results.trace_file)
+    print('Cache Size: ' + str(results.cache_size))
+    print('Block Size: ' + str(results.block_size))
+    print('Associativity: ' + str(results.associativity))
+    print('R-Policy: ' + results.replacement_policy)
+    print()
+
+
+def print_generic_header():
+    print('Generic:')
+    print('Cache Size: ' + str(results.cache_size) + ' KB')
+    print('Block Size: ' + str(results.block_size) + ' bytes')
+    print('Associativity: ' + str(results.associativity))
+    print('Policy: ' + results.replacement_policy)
+
+
+def print_calculated_values():
+    print('----- Calculated Values -----')
+    print('Total #Blocks: ' + str(int(pow(2, results.block_size)/1024)))
+    print('Tag Size: ' + str(tag_size) + ' bits')
+    print('Index Size: ' + str(index_size) + ' bits, Total Indices: ' + str(indices) + ' KB')
+    print('Implementation Memory Size: TBD')
+
+
+def print_results():
+    print('----- Results -----')
+    print('Cache Hit Rate: *** %')
+    print()
+
+
+def parse_file(file):
+    try:
+        with open(file, 'r') as f:
+            line_count = 0
+            for line in f:
+                info = re.match(r'^.+\((\d{2})\).\s{1}(.{8}).+$', line)
+                if info:
+                    print('0x' + info.group(2) + ': (' + str(int(info.group(1))) + ')')
+                line_count += 1
+    except FileNotFoundError:
+        print('Error: File was not found')
+        print('Please check that the file exists and try again')
+        sys.exit(2)
+
+
+# Define the parser arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('-f', action="store", dest="trace_file", type=determine_valid_file)
+parser.add_argument('-s', action="store", dest="cache_size", type=cache_size_type)
+parser.add_argument('-b', action="store", dest="block_size", type=block_size_type)
+parser.add_argument('-a', action="store", dest="associativity", type=int, choices=[1, 2, 4, 8, 16])
+parser.add_argument('-r', action="store", dest="replacement_policy", choices=['RR', 'RND', 'LRU'])
+results = parser.parse_args()
+
+# Calculate values
+block_offset = determine_block_offset()
+index_size = determine_index_size()
+tag_size = 32 - index_size - block_offset
+indices = determine_indices()
+
+# Print the specified headers
+print_formatted_header()
+print_generic_header()
+print_calculated_values()
+print_results()
 
 # Get all files in the data directory and send files to parsing function
-parse_files(get_all_files(sys.argv[1]))
+parse_file(results.trace_file)
