6c623169f8c17cf1d029adaf158015365ad45a97
Rafael Rodriguez <rrodriguez1989@gmail.com>
2019-04-07 13:00:38 -0500

    More print formatting

--- milestone1.py
+++ milestone1.py
@@ -110,6 +110,7 @@ def parse_file(file):
                     if empty != read_write.group(1) and empty != read_write.group(2):
                         print('Data write at: 0x' + read_write.group(1) + ' Data read at : 0x' + read_write.group(2))
                         print('Data write address in binary: ' + bin(int(read_write.group(1), 16))[2:])
+                        print('Data read address in binary: ' + bin(int(read_write.group(2), 16))[2:])
                         print('')
     except FileNotFoundError:
         print('Error: File was not found')
